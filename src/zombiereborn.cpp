707c669a4827946a2a6b831de6afc8eaa7278e86
jiajianet <69965213+jiajianet@users.noreply.github.com>
2024-05-25 20:28:26 +0800

    Update zombiereborn.cpp

--- src/zombiereborn.cpp
+++ src/zombiereborn.cpp
@@ -1391,11 +1391,6 @@ void ZR_InitialInfection()
 			continue;
 		
 		pPlayer->SetImmunity(pPlayer->GetImmunity() - g_iMZImmunityReduction);
-
-		CBasePlayerController* tPlayer = g_playerManager->GetPlayer(i);
-		ClientPrint(tPlayer, HUD_PRINTCENTER, "你是\x04躲藏\x01的人");
-		ClientPrint(tPlayer, HUD_PRINTTALK, ZR_PREFIX "你是\x04躲藏\x01的人, 任务是 20 分钟内\x04不被抓捕的人找到\x01, 抓捕人将在 60 秒后解锁大门开始寻找, 尽全力躲藏和逃亡吧! 祝你好运! ");
-		
 	}
 
 
